580b2d4d71e41b8a01c2f1cdfd3eb67c92c5ccc5
Pedro Tigre <pedrogtigre@gmail.com>
2022-08-11 15:50:26 -0300

    eslint with prettier and other stuff added

--- components/background.js
+++ components/background.js
@@ -3,15 +3,18 @@ import classes from './background.module.css';
 export default function Background() {
   return (
     <div className={classes.clipPath}>
+      <div style={{ position: 'absolute', top: '1.5rem', left: '1.5rem' }}>
+        <Image alt='Logo' src='/images/logo-white.png' width={98} height={50} />
+      </div>
       <div className={classes.bgGradient}>
         <div className={classes.bgWrap}>
           <Image
-            alt="Beautiful river landscape"
-            src="/images/hero.jpg"
-            layout="fill"
-            objectPosition="top"
-            objectFit="cover"
-            priority="true"
+            alt='Beautiful river landscape'
+            src='/images/hero.jpg'
+            layout='fill'
+            objectPosition='top'
+            objectFit='cover'
+            priority='true'
             quality={85}
           />
         </div>
