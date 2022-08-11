a4dc072c13a4003c5bd969d58419a3c4f200d70c
Pedro Tigre <pedrogtigre@gmail.com>
2022-08-11 16:34:45 -0300

    h1 added (not finished)

--- components/background.js
+++ components/background.js
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import classes from './background.module.css';
+import Nav from './nav';
+
 export default function Background() {
   return (
     <div className={classes.clipPath}>
+      <Nav />
       <div style={{ position: 'absolute', top: '1.5rem', left: '1.5rem' }}>
         <Image alt='Logo' src='/images/logo-white.png' width={98} height={50} />
       </div>
@@ -18,7 +21,12 @@ export default function Background() {
             quality={85}
           />
         </div>
-        <p className={classes.bgText}>Some text...</p>
+        <h1 className={classes.bgText}>
+          <span className={classes.headingPrimaryMain}>Outdoors</span>
+          <span className={classes.headingPrimarySub}>
+            is where life happens
+          </span>
+        </h1>
       </div>
     </div>
   );
---
+++ components/nav.js
@@ -0,0 +1,11 @@
+import classes from './nav.module.css';
+
+function Nav() {
+  return (
+    <div className={classes.nav}>
+      <nav>nav</nav>
+    </div>
+  );
+}
+
+export default Nav;
